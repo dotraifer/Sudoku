a461b9393ce2b0f3deef3e7ab788ddee49edffea
Dotan Raifer <dotanraifer@gmail.com>
2022-01-30 03:16:47 +0200

    faild try to naked pairs

--- Board.cs
+++ Board.cs
@@ -74,6 +74,7 @@ namespace Sudoku
         }
         public bool LogicalSolveing(Cell [,] board)
         {
+            int listLength;
             bool has_changed = false;
             for (int i = 0; i < Globals._boardSize; i++)
             {
@@ -81,27 +82,148 @@ namespace Sudoku
                 {
                     if(board[i,j]._value == 0)
                     {
-                        has_changed = FindOnlyPossibility(board, i , j);
+                        if (!has_changed)
+                        {
+                            has_changed = FindOnlyPossibility(board, i, j);
+                            listLength = board[i, j]._possibleNumbers.Count;
+                            foreach(int d in board[i, j]._possibleNumbers)
+                                Console.Write("no" + d);
+                            Console.WriteLine();
+                            if (!has_changed)
+                            {
+                                findGroups(board, i, j, listLength);
+                                has_changed = FindOnlyPossibility(board, i, j);
+
+                            }
+                        }
+                        else
+                        {
+                            FindOnlyPossibility(board, i, j);
+                            listLength = board[i, j]._possibleNumbers.Count;
+                            findGroups(board, i, j, listLength);
+                            FindOnlyPossibility(board, i, j);
+                        }
                     }
                 }
             }
             return has_changed;
         }
+        public bool findGroups(Cell[,] board, int row, int col, int listLength)
+        {
+            bool is_changed = false;
+            int sameListCounterRow = 0;
+            int sameListCounterCol = 0;
+            int sameListCounterBox = 0;
+            int smallBoxSize = (int)Math.Sqrt(Globals._boardSize);
+            // get the coordinates of the first square in the small box
+            int firstBoxRow = row - row % smallBoxSize;
+            int firstBoxColumn = col - col % smallBoxSize;
+            // if each row, col or small box, check if there is other box that the number can be applied to
+            for (int i = 0; i < Globals._boardSize; i++)
+            {
+                // check row
+                if (board[i, col]._value == 0 && i != row)
+                {
+                    if (compareLists(board[row, col]._possibleNumbers, board[i, col]._possibleNumbers))
+                        sameListCounterRow++;
+                }
+                // check col
+                if (board[row, i]._value == 0 && i != col)
+                {
+                    if (compareLists(board[row, col]._possibleNumbers, board[row, i]._possibleNumbers))
+                        sameListCounterCol++;
+                }
+            }
+            // check small box
+            for (int i = firstBoxRow; i < firstBoxRow + smallBoxSize; i++)
+            {
+                for (int j = firstBoxColumn; j < firstBoxColumn + smallBoxSize; j++)
+                {
+                    if (board[i, j]._value == 0 && !(i == row && j == col))
+                    {
+                        if (compareLists(board[row, col]._possibleNumbers, board[row, i]._possibleNumbers))
+                            sameListCounterBox++;
+                    }
+                }
+            }
+            if (sameListCounterRow == listLength)
+            {
+                deleteOptionsFromRow(board, row, board[row, col]._possibleNumbers);
+                is_changed = true;
+            }
+            if (sameListCounterCol == listLength)
+            {
+                deleteOptionsFromRow(board, col, board[row, col]._possibleNumbers);
+                is_changed = true;
+            }
+            if (sameListCounterCol == listLength)
+            {
+                is_changed = true;
+                deleteOptionsFromBox(board, row, col, smallBoxSize, board[row, col]._possibleNumbers);
+            }
+            return is_changed;
+
+        }
+        public bool compareLists(List<int> list1, List<int> list2)
+        {
+            return list1.Equals(list2);
+        }
+        public void deleteOptionsFromRow(Cell[,] board, int row, List<int> toDelete)
+        {
+            for (int i = 0; i < Globals._boardSize; i++)
+            {
+                if (!compareLists(board[row, i]._possibleNumbers, toDelete))
+                    board[row, i]._possibleNumbers = DeleteAllcoincide(board[row, i]._possibleNumbers, toDelete);
+            }
+        }
+        public void deleteOptionsFromCol(Cell[,] board, int col, List<int> toDelete)
+        {
+            for (int i = 0; i < Globals._boardSize; i++)
+            {
+                if (!compareLists(board[i, col]._possibleNumbers, toDelete))
+                    board[i, col]._possibleNumbers = DeleteAllcoincide(board[i, col]._possibleNumbers, toDelete);
+            }
+        }
+        public void deleteOptionsFromBox(Cell[,] board, int row, int col,int boxSize, List<int> toDelete)
+        {
+            for (int i = row; i < row + boxSize; i++)
+            {
+                for (int j = 0; j < col + boxSize; j++)
+                {
+                    if (!compareLists(board[i, j]._possibleNumbers, toDelete))
+                        board[i, j]._possibleNumbers = DeleteAllcoincide(board[i, j]._possibleNumbers, toDelete);
+                }
+            }
+        }
+        public List<int> DeleteAllcoincide(List<int> deletelist, List<int> toDeletelist)
+        {
+            foreach (int number in toDeletelist)
+                deletelist.Remove(number);
+            return deletelist;
+        }
         public bool FindOnlyPossibility(Cell[,] board, int row, int col)
         {
             bool has_changed = false;
+            if (board[row, col]._possibleNumbers.Count == 1)
+            {
+                board[row, col]._value = board[row, col]._possibleNumbers[0];
+                has_changed = true;
+            }
+
             for (int gussed_number = 1; gussed_number <= Globals._boardSize; gussed_number++)
             {
                 if (Solver.IsValid(board, row, col, gussed_number) && DoesOnlyPossible(board, row, col, gussed_number))
                 {
                     board[row, col]._is_constant = true;
                     board[row, col]._value = gussed_number;
+                    board[row, col]._tempValue = gussed_number;
                     has_changed = true;
                     break;
                 }
                 else if(Solver.IsValid(board, row, col, gussed_number))
                 {
-                    board[row, col]._possibleNumbers.Add(gussed_number);
+                    if(!board[row, col]._possibleNumbers.Contains(gussed_number))
+                        board[row, col]._possibleNumbers.Add(gussed_number);
                 }
             }
             return has_changed;
