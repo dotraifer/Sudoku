f95c85d74a040d41e45f339d04a5cc723eae3328
Dotan Raifer <dotanraifer@gmail.com>
2022-02-02 22:44:06 +0200

    some comments added

--- BoardUtils.cs
+++ BoardUtils.cs
@@ -4,15 +4,25 @@ using System.Text;
 
 namespace Sudoku
 {
+
+    /// <summary>
+    /// This class contains the static methods that used on boards
+    /// </summary>
     public static class BoardUtils
     {
 
+        /// <summary>
+        /// static function that takes a board and check if the board is legal/valid
+        /// </summary>
+        /// <param name="board">the board we check if he is valid</param>
+        /// <returns>True if board is valid False otherwise</returns>
         public static bool IsBoardValid(Board board)
         {
             for (int i = 0; i < Globals.BoardSize; i++)
             {
                 for (int j = 0; j < Globals.BoardSize; j++)
                 {
+                    // if not 0 and not valid
                     if (board.Cells[i, j].Value != 0 && !IsValid(board, i, j, board.Cells[i, j].Value))
                         return false;
                 }
@@ -21,7 +31,7 @@ namespace Sudoku
         }
 
         /// <summary>
-        /// this function will check if to put number in the row and col coordinate is valid
+        /// static function that will check if to put number in the row and col coordinate is valid
         /// </summary>
         /// <param name="board">the board state we check on</param>
         /// <param name="row"> the row we check</param>
@@ -37,16 +47,16 @@ namespace Sudoku
                     return false;
                 //check column  
                 if (board.Cells[row, i].Value != 0 && i != col && board.Cells[row, i].Value == number)
-                    return false;
-                //check smaller box block  
+                    return false; 
             }
+            //check smaller box block 
             if (!IsSmallBoxValid(board, row, col, number))
                 return false;
             return true;
         }
 
         /// <summary>
-        /// this function check if the number is already in the small box, or it is possible to put him in the rowand col coorinate
+        /// static function that heck if the number is already in the small box, or it is possible to put him in the rowand col coorinate
         /// </summary>
         /// <param name="board">the board state we check on</param>
         /// <param name="row"> the row we check</param>
@@ -71,12 +81,20 @@ namespace Sudoku
             return true;
 
         }
+
+
+        /// <summary>
+        /// static function that takes a board and check if the board is solved(has no 0)
+        /// </summary>
+        /// <param name="board">the board we check if he is solved</param>
+        /// <returns>True if the board is solver, false otherwise</returns>
         public static bool IsBoardSolved(Board board)
         {
             for (int i = 0; i < board.Cells.GetLength(0); i++)
             {
                 for (int j = 0; j < board.Cells.GetLength(1); j++)
                 {
+                    // if we found 0 - it is not solved
                     if (board.Cells[i, j].Value == 0)
                         return false;
                 }
--- Game.cs
+++ Game.cs
@@ -23,11 +23,14 @@ namespace Sudoku
         /// <returns>grid - the board as a matrix of integers </returns>
         public Cell[,] BuildGrid(string board_string)
         {
+            // if board is empty, or that his length is illegal
             if (board_string.Length == 0 || Math.Sqrt(Math.Sqrt(board_string.Length)) % 1 != 0)
                 throw (new UnpossibleBoardSizeExeption("cant make a board out of " + board_string.Length + " numbers"));
             else
             {
+                // the size of the board
                 Globals.BoardSize = (int)Math.Sqrt(board_string.Length);
+                // the size of the smallbox
                 Globals.SmallBoxSize = (int)Math.Sqrt(Globals.BoardSize);
                 Cell[,] grid = new Cell[Globals.BoardSize, Globals.BoardSize];
                 int arr_counter = 0;
@@ -35,15 +38,15 @@ namespace Sudoku
                 {
                     for (int j = 0; j < Globals.BoardSize; j++)
                     {
+                        // from char to int
                         int value = (int)(board_string[arr_counter] - '0');
+                        // if value is bigger then the biggest possible number
                         if (value < 0 || value > Globals.BoardSize)
                             throw (new InvalidCharException("the char " + board_string[arr_counter] + " is illegal"));
                         else
                         {
-                            if(value == 0)
-                                grid[i, j] = new Cell(value, i, j);
-                            else
-                                grid[i, j] = new Cell(value, i, j);
+                            // put in grid
+                            grid[i, j] = new Cell(value, i, j);
                             arr_counter++;
                         }
                     }
@@ -53,16 +56,31 @@ namespace Sudoku
 
         }
 
+
+        /// <summary>
+        /// this function calls solve the board(calls the solvings methods), then return the result as a string
+        /// </summary>
+        /// <param name="board">the board to solve</param>
+        /// <returns>the string of the result</returns>
         public string SolveBoard(Board board)
         {
+            // if the board is invalid
             if(!BoardUtils.IsBoardValid(board))
                 throw (new InvalidBoardException("board is not valid"));
+            // if the board cannot be solved 
             if (!_board.SolveBoard(board) || !BoardUtils.IsBoardValid(board))
                 throw (new InsolubleBoardException("board is Insoluble"));
-            return ReturnToArray(board.Cells);
+            // return as string
+            return ReturnToString(board.Cells);
         }
 
-        public string ReturnToArray(Cell[,] grid)
+
+        /// <summary>
+        /// this function takes a matrix of int and return him as string
+        /// </summary>
+        /// <param name="grid">the grid to make a string</param>
+        /// <returns>the string of the result</returns>
+        public string ReturnToString(Cell[,] grid)
         {
             string result = "";
             for (int i = 0; i < Globals.BoardSize; i++)
