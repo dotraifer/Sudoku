c15bc082d7cc35fa30dcb8e05286c8fbbabd6a85
Dotan Raifer <dotanraifer@gmail.com>
2022-02-05 17:46:00 +0200

    Final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Made by dotan raifer
+ * 05/02/2021
+ * This project is a sudoku solver, where you enter string from the console or a file, and get the solution for your sudoku
+ * */
+
+
+using System;
 using System.IO;
 using System.Windows.Forms;
 
