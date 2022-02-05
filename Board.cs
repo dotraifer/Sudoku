0acd851737e0cad7410829be00c50c5e8ea1b915
Dotan Raifer <dotanraifer@gmail.com>
2022-02-05 17:12:37 +0200

    Revert "Merge branch 'Dev'"
    This reverts commit 7157a703cd5cc434e41c83bbb1e0df3ccdc96131.

--- Board.cs
+++ Board.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 
 namespace Sudoku
 {
-
-    /// <summary>
-    /// Instance class that represents a board - and containd a matrix of the cells in the board
-    /// </summary>
     public class Board
     {
         private Cell[,] mCells;
@@ -19,21 +15,8 @@ namespace Sudoku
         {
             this.Cells = cells;
         }
-
-
-        /// <summary>
-        ///  this function gets a board and call the functions to solve him
-        /// </summary>
-        /// <remarks>
-        /// the function is called fom the backtracking and in fact recursive
-        /// for every number we guess in the backtracking - we call the function, it calls the logical solver, and then the backtracking, 
-        /// so that for every board we guess we uses the logical solver to see if it has sulotion way faster.
-        /// </remarks>
-        /// <param name="board">the board we want to solve</param>
-        /// <returns>True if the board has a sulotion, else false</returns>
         public bool SolveBoard(Board board)
         {
-<<<<<<< HEAD
             FindPossibaleNumbersValues(board);
             bool has_changed = true;
             while (has_changed)
@@ -41,65 +24,32 @@ namespace Sudoku
                 has_changed = SolvingTactics.LogicalSolveing(board);
             }
             return BackTracking(board);
-=======
-            // the logical solver find new cells values
-            SolvingTactics.LogicalSolveing(board);
-            return GuessNumbers(board);
->>>>>>> Dev
         }
-
-
-        /// <summary>
-        /// this function guesses number in cell that has the smallest number of opptions, and then call the solve function for a copy of the board, 
-        ///that will activate the logical solver, and the will call the backtracking again for the new board
-        /// </summary>
-        /// <param name="board">the board we want to backtrack on</param>
-        /// <returns>True if it found a sulotion, else false</returns>
-        public bool GuessNumbers(Board board)
+        public bool BackTracking(Board board)
         {
             Cell cellChecked = FindLeastOptionsCell(board.Cells);
-            // if there is no cells that still has options, we solve the board or that we got to a deadend(no sulotion)
             if(cellChecked == null)
             {
-<<<<<<< HEAD
                 if(IsBoardSolved(board))
-=======
-                if(BoardUtils.IsBoardSolved(board))
->>>>>>> Dev
                 {
-                    // put in cells the solved board
-                    Cells = board.MatrixCopy();
+                    Cells = board.ArrayCopy();
                     return true;
                 }
                 return false;
             }
-            // if 0
             if (cellChecked.Value == 0)
             {
-                // for every possible num
                 foreach (int possibleNum in cellChecked.PossibleNumbers)
                 {
-<<<<<<< HEAD
                     if (IsValid(board, cellChecked.XLocation, cellChecked.YLocation, possibleNum))
-=======
-                    if (BoardUtils.IsValid(board, cellChecked.XLocation, cellChecked.YLocation, possibleNum))
->>>>>>> Dev
                     {
-                        // create new board
-                        Board newBoard = new Board(board.MatrixCopy());
-                        // put the value in it
+                        Board newBoard = new Board(board.ArrayCopy());
+                        //Board newBoard = new 
                         newBoard.Cells[cellChecked.XLocation, cellChecked.YLocation].Value = possibleNum;
-                        // try to solve the new board
                         if (SolveBoard(newBoard))
                         {
-<<<<<<< HEAD
                             if(!IsBoardSolved(this))
                                 Cells = newBoard.ArrayCopy();
-=======
-                            // if cells matrix isn't solved, copy to cells
-                            if(!BoardUtils.IsBoardSolved(this))
-                                Cells = newBoard.MatrixCopy();
->>>>>>> Dev
                             return true;
                         }
                                 
@@ -109,26 +59,18 @@ namespace Sudoku
             }
             return true;
         }
-
-
-        /// <summary>
-        /// this function copy matrix of Cell by value and return the new Mmtrix 
-        /// </summary>
-        /// <returns>the bnew Cell matrix</returns>
-        public Cell[,] MatrixCopy()
+        public Cell[,] ArrayCopy()
         {
             Cell[,] result = new Cell[Cells.GetLength(0), Cells.GetLength(1)]; //Create a result array that is the same length as the input array
-            for (int x = 0; x < Globals.BoardSize; ++x) 
+            for (int x = 0; x < Cells.GetLength(0); ++x) //Iterate through the horizontal rows of the two dimensional array
             {
-                for (int y = 0; y < Globals.BoardSize; ++y)
+                for (int y = 0; y < Cells.GetLength(1); ++y) //Iterate throught the vertical rows, to add more dimensions add another for loop for z
                 {
-                    //Copy the cell
-                    result[x, y] = new Cell(Cells[x,y].Value, Cells[x, y].XLocation, Cells[x, y].YLocation); 
+                    result[x, y] = new Cell(Cells[x,y].Value, Cells[x, y].XLocation, Cells[x, y].YLocation); //Change result x,y to input x,y
                 }
             }
             return result;
         }
-<<<<<<< HEAD
         public void FindPossibaleNumbersValues(Board board)
         {
             for (int i = 0; i < Globals.BoardSize; i++)
@@ -148,18 +90,8 @@ namespace Sudoku
                 }
             }
         }
-=======
-
-
-        /// <summary>
-        /// this fuction finds the cell with the smallest number of possible opptions, and return it
-        /// </summary>
-        /// <param name="cells">the matrix of cells we want to search in</param>
-        /// <returns>the Cell with smallest number of possible opptions</returns>
->>>>>>> Dev
         public Cell FindLeastOptionsCell(Cell[,] cells)
         {
-            // saves the cell witht he smallest number of possibilities
             Cell minCell = null;
             for (int i = 0; i < Globals.BoardSize; i++)
             {
@@ -176,7 +108,6 @@ namespace Sudoku
             }
             return minCell;
         }
-<<<<<<< HEAD
         public void PrintMatrix(Board board)
         {
             for (int i = 0; i < Globals.BoardSize; i++)
@@ -264,7 +195,5 @@ namespace Sudoku
             }
             return true;
         }
-=======
->>>>>>> Dev
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,112 +1,52 @@
 ﻿using System;
 using System.IO;
-using System.Windows.Forms;
 
 namespace Sudoku
 {
     class Program
     {
-        // path of the file, will be null if we not using file
-        static private string path;
-        [STAThread]
         static void Main(string[] args)
         {
+            string board_string;
             while (true)
             {
-                HandleUser();
-            }
-        }
+                board_string = null;
+                Gui.PrintManu();
+                string manuChoose = Console.ReadLine();
+                manuChoose = manuChoose.ToLower();
+                switch (manuChoose)
+                {
+                    case "console":
+                        Console.WriteLine("~enter a string that would represent the board you want to solve:");
+                        board_string = Console.ReadLine();
+                        break;
+                    case "file":
+                        Console.WriteLine("~enter the text file path:");
+                        board_string = ReadFromFile();
+                        break;
+                    case "exit":
+                        Console.WriteLine("thank you for using!");
+                        Environment.Exit(1);
+                        break;
 
-
-        /// <summary>
-        /// calling the methods nedded for the app to work
-        /// </summary>
-        public static void HandleUser()
-        {
-            // our board to solve
-            string board_string = HandaleUserChoose();
-            var watch = new System.Diagnostics.Stopwatch();
-            watch.Start();
-            if (board_string != null)
-            {
+                }
+                var watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
                 string result_board = Solver.Solve(board_string);
-                watch.Stop();
-                // if nedded to put to file
-                if (path != null && result_board != null)
-                    // append to the file
-                    WriteToFile(result_board);
-                PrintGame(board_string, result_board, watch.ElapsedMilliseconds);
+                // TODO : clean the main
+                if (result_board != null)
+                {
+                    watch.Stop();
+                    Gui.PrintBoard(board_string);
+                    Gui.PrintBoard(result_board);
+                    Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+                }
             }
         }
-        /// <summary>
-        /// static function that get choose from the user, than update the string by the string he put, and return the string
-        /// </summary>
-        /// <returns>the string we want to solve</returns>
-        public static string HandaleUserChoose()
-        {
-            string board_string = null;
-            // is file choosen
-            path = null;
-            Gui.PrintManu();
-            string manuChoose = Console.ReadLine();
-            manuChoose = manuChoose.ToLower();
-            switch (manuChoose)
-            {
-                // if he put in the console
-                case "console":
-                    Console.WriteLine("~enter a string that would represent the board you want to solve:");
-                    board_string = Console.ReadLine();
-                    break;
-                // if he put from file
-                case "file":
-                    board_string = ReadFromFile();
-                    break;
-                // if he want to exit
-                case "exit":
-                    Console.WriteLine("thank you for using!");
-                    Environment.Exit(1);
-                    break;
-                // if none of those
-                default:
-                    Console.WriteLine("unvalid choose");
-                    break;
-
-            }
-            return board_string;
-        }
-
-        /// <summary>
-        /// this function print the game, he print the board we got, the board after solving and the time took to solve
-        /// </summary>
-        /// <remarks>to print the boards, we call the function PrintBoard from the Gui class</remarks>
-        /// <param name="beforeSolve">the board before the solve-as string</param>
-        /// <param name="AfterSolve">the board after the solve-as string</param>
-        /// <param name="timeToSolve">the time it took to solve</param>
-        public static void PrintGame(string beforeSolve, string AfterSolve, long timeToSolve)
-        {
-            if (AfterSolve != null)
-            {
-                Console.WriteLine(AfterSolve);
-                Gui.PrintBoard(beforeSolve);
-                Gui.PrintBoard(AfterSolve);
-               Console.WriteLine($"Execution Time: {timeToSolve} ms");
-            }
-        }
-
-        /// <summary>
-        /// static function that handeles a file Choose of the user
-        /// </summary>
-        /// <remarks>open the file system for the user to choos his file</remarks>
-        /// <returns>the string we got from the file, or null if we got into a problam</returns>
         public static string ReadFromFile()
         {
-            OpenFileDialog fileDialog = new OpenFileDialog
-            {
-                // only txt files
-                Filter = "TXT files|*.txt"
-            };
-            if (fileDialog.ShowDialog() == DialogResult.OK)
-                path = fileDialog.FileName;
+            string path = Console.ReadLine();
+            path = path.Replace("\"", "");
             try {
                 return File.ReadAllText(@path);
             }
@@ -127,22 +67,6 @@ namespace Sudoku
                 Console.WriteLine("unknown problem with file opening\n" + e);
             }
             return null;
-        }
-
-        /// <summary>
-        /// this function writes to the file the solution of the board, 2 lines under the first board
-        /// </summary>
-        /// <param name="resultBoard">the result of the board from the file</param>
-        public static void WriteToFile(string resultBoard)
-        {
-            using (StreamWriter sw = File.AppendText(path))
-            {
-                // spaces
-                sw.WriteLine();
-                sw.WriteLine();
-                // enter the solution to the txt file
-                sw.WriteLine(resultBoard);
-            }
         }
     }
 }
